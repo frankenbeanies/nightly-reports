8975108a45bdd7e557129672131ba45c58a6b196
Samuel Bostick <me@samuelbostick.com>
2017-04-15 09:57:40 -0400

    Added Out of Service Reports

--- src/app/models/nightly-report.ts
+++ src/app/models/nightly-report.ts
@@ -8,6 +8,7 @@ export class NightlyReport {
     public InTomorrow: Report;
     public OutTomorrow: Report;
     public OnLot: Report;
+    public OutOfService: Report;
 
     public constructor(){
         this.Employee = "";
@@ -17,12 +18,14 @@ export class NightlyReport {
         this.InTomorrow = new Report();
         this.OutTomorrow = new Report();
         this.OnLot = new Report();
+        this.OutOfService = new Report();
     }
 
     public ToFormattedString(): string{
         return "In Today%0D%0A%0D%0A" + this.InToday.ToFormatedString()
             + "%0D%0A%0D%0A%0D%0AOut Today%0D%0A%0D%0A" + this.OutToday.ToFormatedString()
             + "%0D%0A%0D%0A%0D%0AIn Tomorrow%0D%0A%0D%0A" + this.InTomorrow.ToFormatedString()
-            + "%0D%0A%0D%0A%0D%0AOut Tomorrow%0D%0A%0D%0A" + this.OutTomorrow.ToFormatedString(); 
+            + "%0D%0A%0D%0A%0D%0AOut Tomorrow%0D%0A%0D%0A" + this.OutTomorrow.ToFormatedString()
+            + "%0D%0A%0D%0A%0D%0AOut of Service%0D%0A%0D%0A" + this.OutOfService.ToFormatedString();
     }
 }
